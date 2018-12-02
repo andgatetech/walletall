f313ad437131cf9f477111e510a903cdec2cf013
Khorshed Alam <andgatetech@gmail.com>
2018-12-02 20:23:54 +0600

    Implement repository

---
+++ app/Repositories/RecurTransactionRepository.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alam
+ * Date: 12/2/18
+ * Time: 8:18 PM
+ */
+
+namespace App\Repositories;
+
+
+class RecurTransactionRepository implements RepositoryInterface
+{
+    public function all()
+    {
+        // TODO: Implement all() method.
+    }
+
+    public function create(array $data)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update(array $data, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function show($id)
+    {
+        // TODO: Implement show() method.
+    }
+
+}
---
+++ app/Repositories/RepositoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alam
+ * Date: 12/2/18
+ * Time: 8:08 PM
+ */
+
+namespace App\Repositories;
+
+
+interface RepositoryInterface
+{
+    public function all();
+
+    public function create(array $data);
+
+    public function update(array $data, $id);
+
+    public function delete($id);
+
+    public function show($id);
+
+}
